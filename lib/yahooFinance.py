dd1c8b7400cb4314fa43882fdb86b730ac3f3138
Jev.Kuznetsov@gmail.com <Jev.Kuznetsov@gmail.com@ed47d5fe-01a5-d6ed-98e1-1a91b84a69b6>
2011-10-28 20:46:59 +0000

    added spread example (not much of an example yet ;-)

--- lib/yahooFinance.py
+++ lib/yahooFinance.py
@@ -7,9 +7,9 @@ Licence: GPL v2
 """
 
 
-from datetime import date
+from datetime import datetime, date
 import urllib2
-import pandas
+from pandas import DataFrame, Index
 import numpy as np
 
 def getHistoricData(symbol, sDate=(2010,1,1),eDate=date.today().timetuple()[0:3]):
@@ -25,37 +25,28 @@ def getHistoricData(symbol, sDate=(2010,1,1),eDate=date.today().timetuple()[0:3]
 
 
     try:
-        fh = urllib2.urlopen(urlStr)
+        lines = urllib2.urlopen(urlStr).readlines()
     except Exception, e:
         s = "Failed to download:\n{0}".format(e);
         print s
 
-    #----------------parse csv data
-
-    h = fh.next().rstrip().split(',')    # split functionality is amazing
-    header = dict(zip(h,range(len(h)))) #create header dictionary
-
-
-    data = [[] for i in range(len(header))] # init data list
-
-    for line in fh:
-        
-        fields = line.rstrip().strip('"').split(',')
-        for i,val in enumerate(fields):
-            try:
-                d = float(val)
-            except (ValueError,):
-                d = val
-            data[i].append(d)
-
-    # convert to numpy array
-    cols = ['Open','High','Low','Close','Adj Close']
-
-    a = np.array([data[header[h]] for h in cols]).transpose()
-
-    #columns=[header[i] for i in idx]
-    df = pandas.DataFrame(a,data[0],columns=[x.lower() for x in cols]).sort_index()
-
+    dates = []
+    data = [[] for i in range(6)]
+    #high
+    
+    # header : Date,Open,High,Low,Close,Volume,Adj Close
+    for line in lines[1:]:
+        fields = line.rstrip().split(',')
+        dates.append(datetime.strptime( fields[0],'%Y-%m-%d'))
+        for i,field in enumerate(fields[1:]):
+            data[i].append(float(field))
+       
+    idx = Index(dates)
+    data = dict(zip(['open','high','low','close','volume','adj_close'],data))
+    
+    # create a pandas dataframe structure   
+    df = DataFrame(data,index=idx).sort()
+    
     return df
 
 

68dc868070ec707c5ceda47defde3b8b18d822d5
jev.kuznetsov@gmail.com <jev.kuznetsov@gmail.com@ed47d5fe-01a5-d6ed-98e1-1a91b84a69b6>
2011-12-05 23:36:14 +0000

    working on spread class

--- trunk/lib/classes.py
+++ trunk/lib/classes.py
@@ -60,43 +60,73 @@ class Spread(object):
     ''' 
     Spread class, used to build a spread out of two symbols.    
     '''
-    def __init__(self,symbolX, symbolY, name=None, capitalX = 100):
+    def __init__(self,df,capital=None, name=None, bet = 100):
         '''
         Init Spread class, with two Symbol objects 
+        
+        Parameters
+        ----------
+        df         : input data frame. First symbol is x, second y
+        capital    : amount of capital on each leg 
+        
         '''
-        if name is None:
-            self.name = symbolX.name+'_'+symbolY.name
+        self.df = df # price data frame
+        self._params = {} # normal properties
+        self._params['samples'] = len(self.df)
         
-        self.symbolX = symbolX
-        self.symbolY = symbolY
-     
+        self._params2 = DataFrame(columns=df.columns) # properties for a matrix
+        self._calculate(capital,bet)
+       
+    def __repr__(self):
+        
+        items =  str.join("\n",['%s:%.2f' % item for item in self._params.items()])
+        
+        return '-'*30+'\n'+items+'\n'+str(self._params2.T)
+         
+    def _calculate(self,capital,bet):
+        ''' internal data calculation, update self._params2 ''' 
+        
+        res = DataFrame(index= self.symbols)
+        res['last close'] = self.df.ix[-1,:]
+        res['beta'] = 1. 
+             
+#        cap = DataFrame(dict(zip(cols,)))
         # set capital ratios
+        if capital is None:
+            beta = estimateBeta(self.df.ix[:,0],self.df.ix[:,1])
+            res['gain'] = [1, -1/beta]
+            res['capital'] = res['gain']*bet
+            res.ix[1,'beta'] = beta
+        else:
+            res['capital'] = capital
         
-        self.beta = estimateBeta(self.symbolX.ohlc['adj_close'], self.symbolY.ohlc['adj_close'])            
-        self.capital = np.array([capitalX,-capitalX/self.beta])      
         
-        #init log
-        self.log = logger.getLogger(self.name)
-        self.log.debug('Spread '+self.name+' created')
+        res['shares'] = res['capital']/res['last close']
+        
+        self._params2 = res
+    
+    @property
+    def spread(self):
+        return (self.df*self._params2['shares']).sum(axis=1)
+   
+    
+    @property
+    def symbols(self):
+        return self.df.columns.tolist()
+    
     
     @property 
-    def dayReturns(self):
+    def returns(self):
+        return (self.df/self.df.shift(1)-1)
+    
+    @property 
+    def spreadReturns(self):
         '''
         Daily returns of the spread
         '''
-        return self.capital[0]*self.symbolX.dayReturns + self.capital[1]*self.symbolY.dayReturns       
+        return (self.returns*self._params2['gain']).sum(axis=1)     
     
-    @property    
-    def X(self):
-        ''' price matrix '''
-        return self.symbolX.histData().join(self.symbolY.histData())
    
-    @property
-    def lastCloseShares(self):
-        ''' amount of shares on last close '''
-        sh = self.capital/self.X.ix[-1,:].values
-        
-        return sh
         
         
 if __name__=='__main__':

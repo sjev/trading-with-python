ed24890705ce6ca25a8e88d6e38e4fae524a3926
jev.kuznetsov@gmail.com <jev.kuznetsov@gmail.com@ed47d5fe-01a5-d6ed-98e1-1a91b84a69b6>
2012-03-11 21:21:36 +0000

    rewritten hist data container to work with HDF5 data.

--- cookbook/cython/testSpeed.py
+++ cookbook/cython/testSpeed.py
@@ -11,17 +11,19 @@ import mean_py
 import mean_c
 import time
 
-a = np.random.rand(10000,5000)
+a = np.random.rand(128,100000).astype(np.float32)
 
 start = time.clock()
 #m1 = a.sum(axis=1)
 #m1 = m1/a.shape[0]
-m1 = a.mean(axis=1)
+m1 = a.mean(axis=0)
 print 'Done in %.3f s' % (time.clock()-start)
+print m1.shape
 
 start = time.clock()
-#m2 = mean_c.mean(a)
+m2 = mean_c.mean(a)
 print 'Done in %.3f s' % (time.clock()-start)
+print m2.shape
 #clf()
 #plot(m1,'b-x')
 #plot(m2,'r-o')

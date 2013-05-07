f4bf0f3e7bfaaad2227213cca37da9e1a87a10d2
jev.kuznetsov@gmail.com <jev.kuznetsov@gmail.com@ed47d5fe-01a5-d6ed-98e1-1a91b84a69b6>
2013-05-07 19:06:15 +0000

    tick logger is now complete. Includes command line arguments

--- cookbook/ib_logQuotes.py
+++ cookbook/ib_logQuotes.py
@@ -3,29 +3,45 @@ Created on May 5, 2013
 Copyright: Jev Kuznetsov
 License: BSD
 
-Demonstration of how to stream quotes from IB
+Program to log tick events to a file
+
+example usage:
+    > python ib_logQuotes.py SPY,VXX,XLE     
+    
+    start with -v option to show all incoming events
+
 
 '''
-import datetime as dt
-import time 
-import os
+
+import argparse # command line argument parser
+import datetime as dt # date and time functions
+import time # time module for timestamping
+import os # used to create directories
+import sys # used to print a dot to a terminal without new line
+
+#--------ibpy imports ----------------------
 from ib.ext.Contract import Contract
 from ib.opt import ibConnection, message
 
 
+# tick type definitions, see IB api manual
 priceTicks = {1:'bid',2:'ask',4:'last',6:'high',7:'low',9:'close', 14:'open'}
 sizeTicks = {0:'bid',3:'ask',5:'last',8:'volume'}
 
 class TickLogger(object):
-    ''' class for handling incoming ticks and saving them to file '''
+    ''' class for handling incoming ticks and saving them to file 
+        will create a subdirectory 'tickLogs' if needed and start logging
+        to a file with current timestamp in its name.
+        All timestamps in the file are in seconds relative to start of logging
+    
+    '''
     def __init__(self,tws, subscriptions):
         ''' init class, register handlers '''
-        tws.registerAll(self._debugHandler)
+      
         tws.register(self._priceHandler,message.TickPrice)
         tws.register(self._sizeHandler,message.TickSize)
         
         self.subscriptions = subscriptions        
-        
         
         # save starting time of logging. All times will be in seconds relative
         # to this moment
@@ -39,10 +55,7 @@ class TickLogger(object):
         print 'Logging ticks to ' , fileName
         self.dataFile = open(fileName,'w')        
     
-    def _debugHandler(self,msg):
-        '''prints data to screen '''
-        print '[msg]:', msg # print message to the screen
-        
+ 
     def _priceHandler(self,msg):
         ''' price tick handler '''
         data = [self.subscriptions[msg.tickerId].m_symbol,'price',priceTicks[msg.field],msg.price] # data, second field is price tick type
@@ -57,14 +70,21 @@ class TickLogger(object):
         ''' write data to log file while adding a timestamp '''
         timestamp = '%.3f' % (time.time()-self._startTime) # 1 ms resolution
         dataLine = ','.join(str(bit) for bit in [timestamp]+data) + '\n'
-        print dataLine
         self.dataFile.write(dataLine)
-        
+   
+    def flush(self):
+        ''' commits data to file'''
+        self.dataFile.flush()
+     
     def close(self):
         '''close file in a neat manner '''
         print 'Closing data file'
         self.dataFile.close()
 
+
+def printMessage(msg):
+    ''' function to print all incoming messages from TWS '''
+    print '[msg]:', msg
 
 
 def createContract(symbol):
@@ -81,7 +101,19 @@ def createContract(symbol):
 
 if __name__ == '__main__':
     
-    symbols = ['SPY','AAPL','XLE']    
+    #-----------parse command line arguments    
+    parser = argparse.ArgumentParser(description='Log ticks for a set of stocks')
+    
+    
+    parser.add_argument("symbols",help = 'symbols separated by coma: SPY,VXX')
+    parser.add_argument("-v", "--verbose", help="show all incoming messages",
+                    action="store_true")    
+    
+    args = parser.parse_args()
+
+    symbols = args.symbols.strip().split(',')
+    print 'Logging ticks for:',symbols
+    
     
     #---create subscriptions  dictionary. Keys are subscription ids   
     subscriptions = {}
@@ -90,15 +122,33 @@ if __name__ == '__main__':
     
     tws = ibConnection()
     logger = TickLogger(tws,subscriptions)
+    
+    # print all messages to the screen if verbose option is chosen
+    if args.verbose:
+        print 'Starting in verbose mode'
+        tws.registerAll(printMessage)
+    
+    
     tws.connect()
 
     #-------subscribe to data
     for subId, c in subscriptions.iteritems():
         tws.reqMktData(subId,c,"",False)
 
-    #-------print data for a couple of seconds, then close
-    time.sleep(15)
+    #------start a loop that must be interrupted with Ctrl-C
+    print 'Press Ctr-C to stop loop'
+
+    try:
+        while True:
+            time.sleep(2) # wait a little
+            logger.flush() # commit data to file
+            sys.stdout.write('.') # print a dot to the screen
                 
-    print 'All done'
+                
+    except KeyboardInterrupt:
+        print 'Interrupted with Ctrl-c'    
+    
     logger.close()             
     tws.disconnect()
+    print 'All done'
+    
